e0c91bdb3ac9364f12f66f283e029b28a1629729
mogax <mogasrx@gmail.com>
2023-05-05 17:22:50 +0200

    Change tslint to eslint

--- src/utils/sessions.ts
+++ src/utils/sessions.ts
@@ -6,7 +6,7 @@ import {
     WordCount, WordPower
 } from "../routes/sessions/sessions.schema";
 
-export const getWordAndExercise: (wordExerciseList:ExerciseWord[], size:number) => ExerciseListWordList = (wordExerciseList:ExerciseWord[], size:number) : ExerciseListWordList => {
+export const getWordAndExercise = (wordExerciseList:ExerciseWord[], size:number) : ExerciseListWordList => {
 
     //Liste des exercices à faire, répartit en différente liste en fonction de leur dépendance entre eux.
     const listExercise1: ExerciseCount[] = [
@@ -55,9 +55,9 @@ export const getWordAndExercise: (wordExerciseList:ExerciseWord[], size:number) 
     const wordLevelList: WordCount[] = groupByWordList(validationList)
 
     //Récupère tous les mots de niveau 1
-    let filteredWordList: WordCount[] = wordLevelList.filter((e:WordCount): boolean => e.count === 1)
+    let filteredWordList: WordCount[] = wordLevelList.filter((e:WordCount)=> e.count === 1)
     if (filteredWordList.length < size) { //Si tous les mots de niveau 1 ne dépasse pas le nombre voulu
-        filteredWordList = [...filteredWordList, ...wordLevelList.filter((e:WordCount): boolean => e.count === 2)] //On ajoute les nombres de niveau 2
+        filteredWordList = [...filteredWordList, ...wordLevelList.filter((e:WordCount) => e.count === 2)] //On ajoute les nombres de niveau 2
     }
     //TODO Possible optimisation, si le nombre de mot ne dépasse pas le nombre de mot voulu les étapes au dessus sont inutiles
     if (filteredWordList.length < size) { //Si tous les mots de niveau 2 ne dépasse pas le nombre voulu
@@ -65,36 +65,32 @@ export const getWordAndExercise: (wordExerciseList:ExerciseWord[], size:number) 
     }
 
     // Récupère les informations des WordExercise avec les id récupérer dans les étapes précédentes
-    const selectedWordList: ExerciseWord[] = [...wordExerciseList.filter((e:ExerciseWord):boolean => filteredWordList.some((a:WordCount): boolean => a.word_list_id === e.word_list_id))]
+    const selectedWordList: ExerciseWord[] = [...wordExerciseList.filter((e:ExerciseWord) => filteredWordList.some((a:WordCount) => a.word_list_id === e.word_list_id))]
 
     //Récupère le groupe de la liste d'exercice n°1, ayant le plus de valeur
-    const selectedExercise1: ExerciseCount = countPowerByExercise(selectedWordList, listExercise1).reduce((acc:ExerciseCount, obj:ExerciseCount):ExerciseCount => obj.totalPower > acc.totalPower ? obj : acc)
+    const selectedExercise1: ExerciseCount = countPowerByExercise(selectedWordList, listExercise1).reduce((acc:ExerciseCount, obj:ExerciseCount) => obj.totalPower > acc.totalPower ? obj : acc)
 
     //Récupère les informations des WordExercises avec les id de la liste d'exercice choisi
-    const selectedWordListByEx1: ExerciseWord[] = wordExerciseList.filter((e:ExerciseWord):boolean => {
-        return selectedExercise1.words.some((w:WordPower) :boolean => {
-            return w.word_list_id === e.word_list_id
-        })
-    })
+    const selectedWordListByEx1: ExerciseWord[] = wordExerciseList.filter((e:ExerciseWord) => selectedExercise1.words.some((w:WordPower) => w.word_list_id === e.word_list_id))
 
     //Récupère le groupe de la liste d'exercice n°2, ayant le plus de valeur avec les mots choisi depuis l'exercice 1
-    const selectedExercise2: ExerciseCount = countPowerByExercise(selectedWordListByEx1, listExercise2).reduce((acc:ExerciseCount, obj:ExerciseCount):ExerciseCount => obj.totalPower > acc.totalPower ? obj : acc)
+    const selectedExercise2: ExerciseCount = countPowerByExercise(selectedWordListByEx1, listExercise2).reduce((acc:ExerciseCount, obj:ExerciseCount) => obj.totalPower > acc.totalPower ? obj : acc)
 
     //On regroupe tous les exercices choisis
     const allExerciseSelected: string[] = [...selectedExercise1.listExercise, ...selectedExercise2.listExercise, '89c180c5-fd2b-4222-98cb-dc7abd455c39']
 
     //On regroupe tous les mots choisis
-    const allWordSelected: string[] = selectedExercise1.words.map((w:WordPower):string => w.word_list_id)
+    const allWordSelected: string[] = selectedExercise1.words.map((w:WordPower) => w.word_list_id)
 
     return {exerciseList:allExerciseSelected, wordList:allWordSelected}
 
     }
 
     //On regroupe les exercices par méthode et par mot et récupérant le nombre de False ou Null
-const countValidationByWordListAndMethod: (data: ExerciseWord[]) => ValidationCount[] = (data: ExerciseWord[]) : ValidationCount[] => {
-    return data.reduce((acc: ValidationCount[], curr: ExerciseWord):ValidationCount[] => {
+const countValidationByWordListAndMethod = (data: ExerciseWord[]) : ValidationCount[] => {
+    return data.reduce((acc: ValidationCount[], curr: ExerciseWord) => {
         if (!curr.validate) { //Si validate est False ou Null
-            const existingCount: ValidationCount | undefined = acc.find((item:ValidationCount):boolean => // On cherche si une validation pour ce mot et cette méthode a déjà été créé
+            const existingCount: ValidationCount | undefined = acc.find((item:ValidationCount) => // On cherche si une validation pour ce mot et cette méthode a déjà été créé
                     item.word_list_id === curr.word_list_id &&
                     item.method_id === curr.exercise.method_id
             )
@@ -113,9 +109,9 @@ const countValidationByWordListAndMethod: (data: ExerciseWord[]) => ValidationCo
 }
 
 //On récupère uniquement le maximum de False ou Null par mot
-const groupByWordList: (data: ValidationCount[]) => WordCount[] = (data: ValidationCount[]) : WordCount[] => {
+const groupByWordList = (data: ValidationCount[]) : WordCount[] => {
     return data.reduce(
-        (acc:WordCount[] , item :ValidationCount): WordCount[] => {
+        (acc:WordCount[] , item :ValidationCount) => {
 
             let index: number = acc.findIndex((e:WordCount):boolean => e.word_list_id === item.word_list_id) // On récupère l'index de l'item dans la liste de WordCount
             if (index === -1) { // Si le mot n'existe pas dans la liste, on le crée
@@ -130,11 +126,11 @@ const groupByWordList: (data: ValidationCount[]) => WordCount[] = (data: Validat
 }
 
 //On remplit pour chaque groupe d'exercice les mot qui correspond avec leur puissance
-const countPowerByExercise: (wordList:ExerciseWord[], exerciseList:ExerciseCount[]) => ExerciseCount[] = (wordList:ExerciseWord[], exerciseList:ExerciseCount[]) : ExerciseCount[] => {
-    wordList.map((exerciseWord:ExerciseWord):void => { //Pour chaque ExerciceWord
-        exerciseList.map((exerciseCount:ExerciseCount): void => { // Pour chaque groupe d'exercice
+const countPowerByExercise = (wordList:ExerciseWord[], exerciseList:ExerciseCount[]) : ExerciseCount[] => {
+    wordList.map((exerciseWord:ExerciseWord) => { //Pour chaque ExerciceWord
+        exerciseList.map((exerciseCount:ExerciseCount) => { // Pour chaque groupe d'exercice
             if((exerciseWord.validate === false || exerciseWord.validate === null) && exerciseCount.listExercise.includes(exerciseWord.exercise_id)){ // On vérifie si l'exercice est False ou Null et s'il se trouve dans le groupe d'exercice
-                let index:number = exerciseCount.words.findIndex((a:WordPower):boolean => a.word_list_id === exerciseWord.word_list_id)  //On cherche l'index du mot dans la liste de mot de l'exercice
+                const index:number = exerciseCount.words.findIndex((a:WordPower) => a.word_list_id === exerciseWord.word_list_id)  //On cherche l'index du mot dans la liste de mot de l'exercice
                 if(index === -1) { // Si le mot n'existe pas, on le crée avec une puissance minimum de 1.
                     exerciseCount.words.push({
                         word_list_id:exerciseWord.word_list_id,
